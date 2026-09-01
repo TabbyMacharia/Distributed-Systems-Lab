26cd38c8d5137fd45ea28f737b70bb588b2ba531
TabbyMacharia <tabithawangari006@gmail.com>
2026-09-01 17:08:05 +0300

    Activity 1, basic server - client communication

--- Experiment-01/Activity-01/server.py
+++ Experiment-01/Activity-01/server.py
@@ -1,1 +1,21 @@
 import socket
+
+server_socket = socket.socket(
+    socket.AF_INET,
+    socket.SOCK_STREAM
+)
+
+server_socket.bind(("localhost", 12345))
+
+server_socket.listen(1)
+
+print("Server is waiting for a connection...")
+
+client_socket, address = server_socket.accept()
+
+print("Client connected:", address)
+
+client_socket.send(b"Hello from server")
+
+client_socket.close()
+server_socket.close()
